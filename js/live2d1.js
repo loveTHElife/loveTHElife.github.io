20cdbea24f2f89c33770fcc60d157970f8b33d8d
yqsy <2156482589@qq.com>
2023-05-29 00:18:30 +0800

    Site updated: 2023-05-29 00:18:30

--- js/live2d1.js
+++ js/live2d1.js
@@ -3,8 +3,8 @@ window.onload = function () {
     var arr = ['myfirst', 'mySecond', 'myThird', 'myFourth', 'myFifth', 'mySixth', 'mySeventh', 'myEighth', 'myNinth', 'myTenth'];
 
     function myTimer() {
-
-        live2d.style.animation = arr[Math.round(Math.random() * 10)] + " 5s linear infinite ";
+        var num = Math.round(Math.random() * 5) + 5;
+        live2d.style.animation = arr[Math.round(Math.random() * 10)] + " " + num + "s linear infinite ";
     }
     //切换动画第一first、第二second 、第三third、第四fourth 、第五fifth 、第六sixth 、第七seventh
     //第八eighth ,第九ninth 、第十tenth
